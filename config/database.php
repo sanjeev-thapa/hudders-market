58fd266942fb6ac1a2be339e211f6f2b615ba924
sanjeev-thapa <stedwardson@gmail.com>
2021-07-12 22:52:57 +0545

    Add TNS for Oracle Database

--- config/database.php
+++ config/database.php
@@ -97,6 +97,7 @@ return [
             'port' => env('DB_PORT', '1521'),
             'database' => env('DB_DATABASE', ''),
             'service_name' => env('DB_SERVICE', ''),
+            'tns' => env('DB_TNS', ''),
             'username' => env('DB_USERNAME', ''),
             'password' => env('DB_PASSWORD', ''),
             'charset' => '',
